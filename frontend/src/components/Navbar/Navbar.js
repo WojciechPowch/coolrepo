0aabbd87887741af07b0af76c8b7ed1727a62dcd
Jakub Salamon <jakubsalamon@o2.pl>
2019-11-27 05:19:58 +0100

    added nav logic

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
+    const [link, setLink] = useState({
+        home: "navbar__link",
+        info: "navbar__link",
+        news: "navbar__link",
+        contact: "navbar__link"
+    });
+
+
+    const navClickHandler = (e) => {
+        const navPosition = e.target.attributes[1].nodeValue;
+        const tempObj = {
+            home: "navbar__link",
+            info: "navbar__link",
+            news: "navbar__link",
+            contact: "navbar__link"
+        }
+        tempObj[navPosition] = `navbar__link--${navPosition}`;
+        setLink(tempObj);
+    }
+
     return (
         <section className="navbar">
-            <Link className="navbar__link" to="/">Home</Link>
-            <Link className="navbar__link" to="/information">O nas</Link>
-            <Link className="navbar__link" to="/news">Wiadomości</Link>
-            <Link className="navbar__link" to="/contact">Kontakt</Link>
+            <Link onClick={navClickHandler} className={link.home} navposition={"home"} to="/">Home</Link>
+            <Link onClick={navClickHandler} className={link.info} navposition={"info"} to="/information">O nas</Link>
+            <Link onClick={navClickHandler} className={link.news} navposition={"news"} to="/news">Wiadomości</Link>
+            <Link onClick={navClickHandler} className={link.contact} navposition={"contact"} to="/contact">Kontakt</Link>
         </section>
     )
 
